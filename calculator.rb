16f008af354216d8a1eb641727dd18fa33da99af
karan <bamniyakaran71@gmail.com>
2025-02-14 04:36:12 +0000

    CAL-016 Added code for the Refactor of code for Negative numbers

--- calculator.rb
+++ calculator.rb
@@ -2,10 +2,11 @@ class Calculator
   def self.string_addition(string_number)
     string_number = separator_and_string(string_number)
     values = string_number.split(',').map(&:to_i)
-    negative_numbers = values.select(&:negative?)
-    raise "negative numbers not allowed #{negative_numbers.join(",")}" if !negative_numbers.nil? && negative_numbers.any?
+    check_negative_number(values)
     return values.sum
   end
+
+  private
 
   def self.separator_and_string(str)
     if str.start_with?("//")
@@ -16,5 +17,10 @@ class Calculator
       separator = /\n/
     end
     return str.gsub(separator, ',')
+  end
+
+  def self.check_negative_number(values)
+    negative_numbers = values.select(&:negative?)
+    raise "negative numbers not allowed #{negative_numbers.join(",")}" if !negative_numbers.nil? && negative_numbers.any?
   end
 end
