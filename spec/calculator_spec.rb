6c25deae5cf494a32df29d87dceb4b542ad1d6e8
karan <bamniyakaran71@gmail.com>
2025-02-14 01:44:43 +0000

    CAL-003 Added code for the Test Case Fails for Empty String

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -3,6 +3,8 @@ require_relative '../calculator'
 
 RSpec.describe Calculator do
   describe '#add values' do
-    
+    it 'returns 0 for an empty string' do
+      expect(Calculator.string_addition("")).to eq(0)
+    end
   end
 end
