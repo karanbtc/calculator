4faf6b688fce746612ad18e7a69fd0ce97684eec
karan <bamniyakaran71@gmail.com>
2025-02-14 01:37:25 +0000

    CAL-002 Added code for the File/Class Basic Setup

---
+++ calculator.rb
@@ -0,0 +1,5 @@
+class Calculator
+  def self.string_addition(string)
+
+  end
+end 
---
+++ spec/calculator_spec.rb
@@ -0,0 +1,8 @@
+require 'rspec'
+require_relative '../calculator'
+
+RSpec.describe Calculator do
+  describe '#add values' do
+    
+  end
+end
